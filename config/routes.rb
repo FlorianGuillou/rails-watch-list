dee02d665a9e90a25316f8cf2bbee4974be17f24
Florian Guillou <guilflo@hotmail.fr>
2022-11-25 10:11:15 -0600

    add dotenv-rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
   root to: 'lists#index'
   resources :lists, only: %i[index new create show] do
-    resources :bookmarks , only: %i[new create]
+    resources :bookmarks, only: %i[new create]
   end
   resources :bookmarks, only: %i[destroy]
 end
